298ebf14f0eb2860ea1c0d3981d7ff1668269d32
mihansweatpants <mihansweatpants@gmail.com>
2019-07-09 23:38:52 +0300

    HT1.1

--- admin/src/ducks/auth.js
+++ admin/src/ducks/auth.js
@@ -1,5 +1,5 @@
-import {appName} from '../config'
-import {Record} from 'immutable'
+import { appName } from '../config'
+import { Record } from 'immutable'
 import firebase from 'firebase/app'
 
 /**
@@ -16,36 +16,39 @@ export const SIGN_UP_START = `${prefix}/SIGN_UP_START`
 export const SIGN_UP_SUCCESS = `${prefix}/SIGN_UP_SUCCESS`
 export const SIGN_UP_ERROR = `${prefix}/SIGN_UP_ERROR`
 
+export const SIGN_OUT_SUCCESS = `${prefix}/SIGN_OUT_SUCCESS`
+
 /**
  * Reducer
  * */
 export const ReducerRecord = Record({
-    user: null,
-    error: null,
-    loading: null
+  user: null,
+  error: null,
+  loading: null
 })
 
 export default function reducer(state = new ReducerRecord(), action) {
-    const {type, payload, error} = action
+  const { type, payload, error } = action
 
-    switch (type) {
-        case SIGN_IN_START:
-        case SIGN_UP_START:
-            return state.set('loading', true)
+  switch (type) {
+    case SIGN_IN_START:
+    case SIGN_UP_START:
+      return state.set('loading', true)
 
-        case SIGN_IN_SUCCESS:
-        case SIGN_UP_SUCCESS:
-            return state.set('loading', false)
-                .set('user', payload.user)
+    case SIGN_IN_SUCCESS:
+    case SIGN_UP_SUCCESS:
+      return state.set('loading', false).set('user', payload.user)
 
-        case SIGN_IN_ERROR:
-        case SIGN_UP_ERROR:
-            return state.set('loading', false)
-                .set('error', error)
+    case SIGN_IN_ERROR:
+    case SIGN_UP_ERROR:
+      return state.set('loading', false).set('error', error)
 
-        default:
-            return state
-    }
+    case SIGN_OUT_SUCCESS:
+      return state.set('user', null)
+
+    default:
+      return state
+  }
 }
 
 /**
@@ -57,49 +60,49 @@ export default function reducer(state = new ReducerRecord(), action) {
  * */
 
 export function signIn(email, password) {
-    return async (dispatch) => {
-        dispatch({
-            type: SIGN_IN_START
-        })
+  return async (dispatch) => {
+    dispatch({
+      type: SIGN_IN_START
+    })
 
-        try {
-            const user = await firebase.auth().signInWithEmailAndPassword(email, password)
+    try {
+      const user = await firebase
+        .auth()
+        .signInWithEmailAndPassword(email, password)
 
-            dispatch({
-                type: SIGN_IN_SUCCESS,
-                payload: {user}
-            })
-        } catch (error) {
-            dispatch({
-                type: SIGN_IN_ERROR,
-                error
-            })
-        }
+      dispatch({
+        type: SIGN_IN_SUCCESS,
+        payload: { user }
+      })
+    } catch (error) {
+      dispatch({
+        type: SIGN_IN_ERROR,
+        error
+      })
     }
+  }
 }
 
 export function signUp(email, password) {
-    return async (dispatch) => {
-        dispatch({
-            type: SIGN_UP_START
-        })
+  return async (dispatch) => {
+    dispatch({
+      type: SIGN_UP_START
+    })
 
-        try {
-            const user = await firebase.auth().createUserWithEmailAndPassword(email, password)
+    try {
+      const user = await firebase
+        .auth()
+        .createUserWithEmailAndPassword(email, password)
 
-            dispatch({
-                type: SIGN_UP_SUCCESS,
-                payload: {user}
-            })
-        } catch (error) {
-            dispatch({
-                type: SIGN_UP_ERROR,
-                error
-            })
-        }
+      dispatch({
+        type: SIGN_UP_SUCCESS,
+        payload: { user }
+      })
+    } catch (error) {
+      dispatch({
+        type: SIGN_UP_ERROR,
+        error
+      })
     }
+  }
 }
-
-firebase.auth().onAuthStateChanged(user => {
-    console.log('Auth state changed:', user)
-})
--- admin/src/redux/index.js
+++ admin/src/redux/index.js
@@ -1,13 +1,26 @@
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import { routerMiddleware } from 'connected-react-router'
 import logger from 'redux-logger'
 import thunk from 'redux-thunk'
+import firebase from 'firebase/app'
 import reducer from './reducer'
 import history from '../history'
+import { SIGN_IN_SUCCESS, SIGN_OUT_SUCCESS } from '../ducks/auth'
 
-const enhancer = applyMiddleware(thunk, routerMiddleware(history), logger)
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const enhancer = composeEnhancers(
+  applyMiddleware(thunk, routerMiddleware(history), logger)
+)
 
 const store = createStore(reducer, enhancer)
+
+firebase.auth().onAuthStateChanged((user) => {
+  if (user) {
+    store.dispatch({ type: SIGN_IN_SUCCESS, payload: { user } })
+  } else {
+    store.dispatch({ type: SIGN_OUT_SUCCESS })
+  }
+})
 
 //dev only
 window.store = store
