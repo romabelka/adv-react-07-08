99a98d97e34a30a94c1667e93165ff30f8364371
Roman Iakobchuk <kaboom.ua@gmail.com>
2019-07-11 17:37:11 +0300

    add mocks

--- admin/src/components/auth/sign-in-form.js
+++ admin/src/components/auth/sign-in-form.js
@@ -1,19 +1,18 @@
 import React from 'react'
-import {reduxForm, Field} from 'redux-form'
+import { reduxForm, Field } from 'redux-form'
 
 function SignInForm({ handleSubmit }) {
-    return (
-        <form onSubmit={handleSubmit}>
-            Email: <Field name="email" component="input" />
-            Password: <Field name="password" component="input" type="password"/>
-            <button>Sign In</button>
-        </form>
-    )
+  return (
+    <form onSubmit={handleSubmit}>
+      Email: <Field name="email" component="input" />
+      Password: <Field name="password" component="input" type="password" />
+      <button>Sign In</button>
+    </form>
+  )
 }
 
-SignInForm.propTypes = {
-}
+SignInForm.propTypes = {}
 
 export default reduxForm({
-    form: 'sign-in'
+  form: 'sign-in'
 })(SignInForm)
--- admin/src/components/routes/auth.js
+++ admin/src/components/routes/auth.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react'
 import { NavLink, Route } from 'react-router-dom'
-import {connect} from 'react-redux'
-import SignInForm from "../auth/sign-in-form";
-import SignUpForm from "../auth/sign-up-form";
-import {signIn, signUp} from "../../ducks/auth";
+import { connect } from 'react-redux'
+import SignInForm from '../auth/sign-in-form'
+import SignUpForm from '../auth/sign-up-form'
+import { signIn, signUp } from '../../ducks/auth'
 
 class AuthPage extends Component {
   static propTypes = {}
@@ -13,22 +13,29 @@ class AuthPage extends Component {
       <div>
         <h1>Auth Page</h1>
         <div>
-          <NavLink activeStyle={{ color: 'red' }} to="/auth/sign-in">sign in</NavLink>
+          <NavLink activeStyle={{ color: 'red' }} to="/auth/sign-in">
+            sign in
+          </NavLink>
         </div>
         <div>
-          <NavLink activeStyle={{ color: 'red' }} to="/auth/sign-up">sign up</NavLink>
+          <NavLink activeStyle={{ color: 'red' }} to="/auth/sign-up">
+            sign up
+          </NavLink>
         </div>
-        <Route path="/auth/sign-in" render = {this.signInForm}/>
-        <Route path="/auth/sign-up" render = {this.signUpForm}/>
+        <Route path="/auth/sign-in" render={this.signInForm} />
+        <Route path="/auth/sign-up" render={this.signUpForm} />
       </div>
     )
   }
 
-  signInForm = () => <SignInForm onSubmit={this.handleSignIn}/>
-  signUpForm = () => <SignUpForm onSubmit={this.handleSignUp}/>
+  signInForm = () => <SignInForm onSubmit={this.handleSignIn} />
+  signUpForm = () => <SignUpForm onSubmit={this.handleSignUp} />
 
   handleSignIn = ({ email, password }) => this.props.signIn(email, password)
-    handleSignUp = ({ email, password }) => this.props.signUp(email, password)
+  handleSignUp = ({ email, password }) => this.props.signUp(email, password)
 }
 
-export default connect(null, { signIn, signUp })(AuthPage)
+export default connect(
+  null,
+  { signIn, signUp }
+)(AuthPage)
