2c6e6878795520bce56f15d6f86de85f1eb228f4
Vladyslav Kulyk <vladyslav.kulyk@gmail.com>
2019-07-15 00:47:08 +0300

    HT2.4 Сохранять и читать людей в/из FB

--- admin/src/ducks/people.js
+++ admin/src/ducks/people.js
@@ -4,6 +4,7 @@ import { reset } from 'redux-form'
 import { createSelector } from 'reselect'
 import { put, takeEvery, call } from 'redux-saga/effects'
 import { generateId } from '../services/utils'
+import { savePersonToFB, getPersonsFromFB } from '../mocks/index'
 
 /**
  * Constants
@@ -12,6 +13,11 @@ export const moduleName = 'people'
 const prefix = `${appName}/${moduleName}`
 export const ADD_PERSON = `${prefix}/ADD_PERSON`
 export const ADD_PERSON_REQUEST = `${prefix}/ADD_PERSON_REQUEST`
+export const ADD_PERSON_ERROR = `${prefix}/ADD_PERSON_ERROR`
+
+export const GET_PEOPLE = `${prefix}/GET_PEOPLE`
+export const GET_PEOPLE_REQUEST = `${prefix}/GET_PEOPLE_REQUEST`
+export const GET_PEOPLE_ERROR = `${prefix}/GET_PEOPLE_ERROR`
 
 /**
  * Reducer
@@ -32,8 +38,12 @@ export default function reducer(state = new ReducerState(), action) {
 
   switch (type) {
     case ADD_PERSON:
-      return state.update('entities', (entities) =>
+      return state.update('entities', (entities) => {
         entities.push(new PersonRecord(payload))
+      })
+    case GET_PEOPLE:
+      return state.update('entities', (entities) =>
+        payload.map((person) => entities.push(new PersonRecord(person)))
       )
 
     default:
@@ -61,6 +71,12 @@ export function addPerson(person) {
   }
 }
 
+export function getPeople() {
+  return {
+    type: GET_PEOPLE_REQUEST
+  }
+}
+
 /**
  * Sagas
  **/
@@ -68,17 +84,51 @@ export function addPerson(person) {
 export function* addPersonSaga(action) {
   const id = yield call(generateId)
 
-  yield put({
-    type: ADD_PERSON,
-    payload: {
-      id,
-      ...action.payload.person
-    }
-  })
+  try {
+    yield call(savePersonToFB, action.payload.person)
+
+    yield put({
+      type: ADD_PERSON,
+      payload: {
+        id,
+        ...action.payload.person
+      }
+    })
+  } catch (error) {
+    yield put({
+      type: ADD_PERSON_ERROR,
+      error
+    })
+  }
 
   yield put(reset('person'))
 }
 
+export function* getPeopleSaga() {
+  try {
+    const people = yield call(getPersonsFromFB)
+
+    const peopleWithID = people.map((person) => {
+      const id = generateId()
+      return {
+        id,
+        ...person.data()
+      }
+    })
+
+    yield put({
+      type: GET_PEOPLE,
+      payload: peopleWithID
+    })
+  } catch (error) {
+    yield put({
+      type: GET_PEOPLE_ERROR,
+      error
+    })
+  }
+}
+
 export function* saga() {
   yield takeEvery(ADD_PERSON_REQUEST, addPersonSaga)
+  yield takeEvery(GET_PEOPLE_REQUEST, getPeopleSaga)
 }
