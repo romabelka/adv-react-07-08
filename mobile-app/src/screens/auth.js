75d69fb679dbaa625dc472f2b649457c3925ae4d
Roman Iakobchuk <kaboom.ua@gmail.com>
2019-08-05 19:23:27 +0300

    add different platforms

--- mobile-app/src/screens/auth.js
+++ mobile-app/src/screens/auth.js
@@ -1,21 +1,43 @@
 import React from 'react'
-import {Text, View, TextInput, Button} from 'react-native'
+import {Text, View, TextInput, Button, Platform} from 'react-native'
+import AuthHeading from '../components/auth-heading'
 
 export default function AuthScreen() {
     return (
         <View>
-            <Text>Sign In:</Text>
+            <AuthHeading />
             <View>
-                <Text>Email:</Text>
-                <TextInput />
+                <Text style={{
+                    fontSize: 20,
+                    color: '#ff0000'
+                }}>Email:</Text>
+                <TextInput style={styles.input} />
             </View>
             <View>
-                <Text>Password:</Text>
-                <TextInput />
+                <Text style={styles.text}>Password:</Text>
+                <TextInput  style={styles.input}/>
             </View>
             <View>
                 <Button onPress={() => console.log('sign in')} title="Sign In"/>
             </View>
         </View>
     )
+}
+
+const styles = {
+    text: {
+        fontSize: 20,
+        color: 'red'
+    },
+    input: {
+        borderBottomWidth: 1,
+        ...Platform.select({
+            ios: {
+                borderBottomColor: 'green'
+            },
+            android: {
+                borderBottomColor: 'blue'
+            }
+        })
+    }
 }
